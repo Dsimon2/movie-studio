7278b113c04c4aa9d60df445abae069556e9b62e
Dsimon2 <browsedavid@gmail.com>
2023-08-14 13:20:53 -0400

    usestate created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react"
+import "./Home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
+
+const Home = () => {
+
+    const [ Movies, setMovies ] = useState([])
+
+}
+
+    export default Home
